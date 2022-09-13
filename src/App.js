733cc9cfbcd8464a2051156c861fb2c7576c6733
heyitsuzair <uzairdevil354123@gmail.com>
2022-09-14 00:00:30 +0500

    Final Push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Typography, AppBar } from "@material-ui/core";
+import VideoPlayer from "./components/VideoPlayer";
+import Options from "./components/Sidebar";
+import Notifications from "./components/Notifications";
+import { makeStyles } from "@material-ui/styles";
 
 function App() {
+  const useStyles = makeStyles((theme) => ({
+    appBar: {
+      borderRadius: 15,
+      margin: "30px 100px",
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "center",
+      alignItems: "center",
+      width: "600px",
+      border: "2px solid black",
+    },
+    image: {
+      marginLeft: "15px",
+    },
+    wrapper: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      width: "100%",
+    },
+  }));
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AppBar position="static" className={classes.appBar} color="inherit">
+        <Typography variant="h2" align="center">
+          Zoom Clone
+        </Typography>
+      </AppBar>
+      <VideoPlayer />
+      <Options>
+        <Notifications />
+      </Options>
     </div>
   );
 }

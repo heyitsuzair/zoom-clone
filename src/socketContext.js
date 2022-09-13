9537abed4dd3ce1a2c32e9effb50c601165fcdff
heyitsuzair <uzairdevil354123@gmail.com>
2022-09-14 00:47:14 +0500

    Final Push

--- src/socketContext.js
+++ src/socketContext.js
@@ -4,7 +4,7 @@ import Peer from "simple-peer";
 
 const SocketContext = createContext();
 
-const socket = io(process.env.io || "http://localhost:5000");
+const socket = io(process.env.REACT_APP_io || "http://localhost:5000");
 
 const ContextProvider = ({ children }) => {
   const [callAccepted, setCallAccepted] = useState(false);
